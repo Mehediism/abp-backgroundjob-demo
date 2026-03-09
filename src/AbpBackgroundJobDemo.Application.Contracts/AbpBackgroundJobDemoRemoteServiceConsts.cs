a78a086b2b0514b8040b13dd91c0760ef9009f51
Mehedi <86913265+Mehediism@users.noreply.github.com>
2026-03-09 16:07:11 +0600

    Add remote service consts

---
+++ src/AbpBackgroundJobDemo.Application.Contracts/AbpBackgroundJobDemoRemoteServiceConsts.cs
@@ -0,0 +1,7 @@
+namespace AbpBackgroundJobDemo;
+
+public static class AbpBackgroundJobDemoRemoteServiceConsts
+{
+    public const string RemoteServiceName = "AbpBackgroundJobDemo";
+    public const string ModuleName = "abpBackgroundJobDemo";
+}
